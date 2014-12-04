c72f99b68500543de2f3f648c92a255424840564
Charlie Franklin <charlie.franklin@me.com>
2014-12-04 12:24:08 +1100

    Starting putting it all together

--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var howManyBalloonsLabel: UILabel!
 
+    var myBalloons:[Balloon] = []
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
